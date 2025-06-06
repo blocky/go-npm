83de738d9754f219e2fd1bcf88dfa7598eb3ca68
Mike Wittie <mwittie@blocky.rocks>
2025-06-06 14:52:26 -0600

    Symlink the downloaded binary instead of moving it

--- src/index.js
+++ src/index.js
@@ -58,8 +58,8 @@ function verifyAndPlaceBinary(binName, binPath, callback) {
     getInstallationPath(function(err, installationPath) {
         if (err) return callback("Error getting binary installation path from `npm bin`");
 
-        // Move the binary file
-        fs.renameSync(path.join(binPath, binName), path.join(installationPath, binName));
+        // Create a symbolic link instead of moving or copying the file
+        fs.symlinkSync(path.join(binPath, binName), path.join(installationPath, binName));
 
         callback(null);
     });
